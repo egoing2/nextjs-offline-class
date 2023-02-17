a036560e2635b6f236f7d7171676b3a319417de2
egoing <egoing@gmail.com>
2023-02-17 19:28:48 +0900

    Link를 이용해서 리로드 없이 페이지 전환

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,21 +1,22 @@
+import Link from 'next/link';
 export function Layout({ children }) {
   return <>
     <header>
-      <h1><a href="/">WEB</a></h1>
+      <h1><Link href="/">WEB</Link></h1>
     </header>
     <nav>
       <ol>
-        <li><a href="/read/1">html</a></li>
-        <li><a href="/read/2">css</a></li>
+        <li><Link href="/read/1">html</Link></li>
+        <li><Link href="/read/2">css</Link></li>
       </ol>
     </nav>
     <article>
       {children}
     </article>
     <ul>
-      <li><a href="/create">Create</a></li>
-      <li><a href="/update">Update</a></li>
-      <li><a href="/delete">Delete</a></li>
+      <li><Link href="/create">Create</Link></li>
+      <li><Link href="/update">Update</Link></li>
+      <li><Link href="/delete">Delete</Link></li>
     </ul>
   </>;
 }
